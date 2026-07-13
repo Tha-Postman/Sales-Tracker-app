69213a2b9057e6f860f57c78b863bf7de976dd70
Oluwatobiloba <olaniyanoluwatobi01@gmail.com>
2026-07-13 12:12:02 -0400

    update

--- server.js
+++ server.js
@@ -144,22 +144,6 @@ function getPasswordPolicyMessage(password) {
         return "Password must be at least 8 characters long.";
     }
 
-    if (!/[a-z]/.test(password)) {
-        return "Password must include at least one lowercase letter.";
-    }
-
-    if (!/[A-Z]/.test(password)) {
-        return "Password must include at least one uppercase letter.";
-    }
-
-    if (!/[0-9]/.test(password)) {
-        return "Password must include at least one number.";
-    }
-
-    if (!/[^A-Za-z0-9]/.test(password)) {
-        return "Password must include at least one symbol, like @, #, !, or $.";
-    }
-
     return "";
 }
 
